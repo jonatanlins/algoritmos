0b58ea27f6fe278f1af8a5962c1edc76b74ae847
Jonatan Lins <jonatanpinheiro@outlook.com>
2018-08-16 20:49:19 -0300

    Cifra de césar adicionada em python

---
+++ python/URI Online Judge/Cifra de César.py
@@ -0,0 +1,15 @@
+# Cifra de César
+# Problema 1253 do URI Judge Online
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1253
+
+inputs = int(input())
+
+for i in range(inputs):
+  text = list(input())
+  shift = int(input())
+
+  text = [ ord(c) - shift for c in text ]
+  text = [ chr(c) if c >= 65 else chr(c + 26) for c in text ]
+  text = ''.join(text)
+
+  print(text)
