3f901840e0e583b22f767a9162de0d7d7d2f2584
Jonatan Lins <jonatanpinheiro@outlook.com>
2018-08-19 14:23:08 -0300

    adidionado 1001 em python

---
+++ python/1001.py
@@ -0,0 +1,7 @@
+# Extremamente Básico
+# Problema 1001 do URI Online Judge
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1001
+
+x = int(input()) + int(input())
+
+print('X = {}'.format(x))
