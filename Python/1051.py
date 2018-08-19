5ea30c53e4665b6003e61c8e628e54cd6a8d5465
Jonatan Lins <jonatanpinheiro@outlook.com>
2018-08-19 13:01:07 -0300

    adicionada questão "Imposto de Renda" em python

---
+++ Python/1051.py
@@ -0,0 +1,14 @@
+# Imposto de Renda
+# Problema 1051 do URI Judge Online
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1051
+
+value = float(input())
+
+if value <= 2000:
+  print('Isento')
+elif value <= 3000:
+  print('R$ {:.2f}'.format((value - 2000) * 0.08))
+elif value <= 4500:
+  print('R$ {:.2f}'.format((value - 3000) * 0.18 + 1000 * 0.08))
+else:
+  print('R$ {:.2f}'.format((value - 4500) * 0.28 + 1000 * 0.08 + 1500 * 0.18))
