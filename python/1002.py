c930b00418fb54f1ffade8b9262652b8d450a03d
Jonatan Lins <jonatanpinheiro@outlook.com>
2018-08-19 14:23:17 -0300

    adicionado 1002 em python

---
+++ python/1002.py
@@ -0,0 +1,7 @@
+# Área do Círculo
+# Problema 1002 do URI Online Judge
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1002
+
+raio = float(input())
+
+print('A={:.4f}'.format(3.14159 * raio ** 2))
