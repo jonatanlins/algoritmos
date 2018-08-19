e052e1bd33c8551e3e743a74d1d5bc777007485e
Jonatan Lins <jonatanpinheiro@outlook.com>
2018-08-19 14:23:27 -0300

    adicionado 1003 em python

---
+++ python/1003.py
@@ -0,0 +1,7 @@
+# Soma Simples
+# Problema 1003 do URI Online Judge
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1003
+
+soma = int(input()) + int(input())
+
+print('SOMA = {}'.format(soma))
