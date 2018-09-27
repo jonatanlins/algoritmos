5649c8aca7e5ec9f9a892f011d187b2900f45a7e
Jonatan Lins <jonatanpinheiro@outlook.com>
2018-09-27 17:55:32 -0300

    feito 1016 em python

---
+++ problemas/1016.py
@@ -0,0 +1,12 @@
+# Distância
+# Problema 1016 do URI Online Judge
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1016
+
+def main ():
+  distance = int(input())
+
+  print('{} minutos'.format(distance * 2))
+
+
+if __name__ == '__main__':
+  main()
