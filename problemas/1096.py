72841fb840f8d023b04ca1b8fea94b20cb47132b
Jonatan Lins <jonatanpinheiro@outlook.com>
2018-09-27 21:59:57 -0300

    feito 1096 em python

---
+++ problemas/1096.py
@@ -0,0 +1,12 @@
+# Sequencia IJ 2
+# Problema 1096 do URI Online Judge
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1096
+
+def main ():
+  for i in range(1, 10, 2):
+    for j in range(7, 4, -1):
+      print('I={} J={}'.format(i, j))
+
+
+if __name__ == '__main__':
+  main()
