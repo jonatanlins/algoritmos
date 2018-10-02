e32376457035364cf1b0995c7bab1671128eab7d
Jonatan Lins <jonatanpinheiro@outlook.com>
2018-10-01 22:34:27 -0300

    feito 1059 em python

---
+++ problemas/1059.py
@@ -0,0 +1,11 @@
+# Números Pares
+# Problema 1059 do URI Online Judge
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1059
+
+def main ():
+  for i in range(2, 101, 2):
+    print(i)
+
+
+if __name__ == '__main__':
+  main()
